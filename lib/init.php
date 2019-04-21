ce668d978670c56d6d24b4f39515b0b471adfffa
Tristan Bagot <tristan.bagot@gmail.com>
2019-04-21 12:18:12 +0200

    fix

--- lib/init.php
+++ lib/init.php
@@ -2,17 +2,18 @@
 
 if (c::get('kirby.vimeosync')) {
 
-  if (c::get('kirby.vimeosync.access_token')) {
-      throw new Exception(
-          'You can not upload a file without an access token. You can find this token on your app page, or generate ' .
-          'one using `auth.php`.'
-      );
-  }
-
-  require_once __DIR__ . DS . 'methods.php';
+    if (!c::get('kirby.vimeosync.access_token')) {
+        throw new Exception(
+            'You can not upload a file without an access token. You can find this token on your app page, or generate ' .
+            'one using `auth.php`.'
+        );
+        require_once __DIR__ . DS . 'fallback.php';
+    } else {
+        require_once __DIR__ . DS . 'methods.php';
+    }
 
 } else {
 
-  require_once __DIR__ . DS . 'fallback.php';
+    require_once __DIR__ . DS . 'fallback.php';
 
 }
